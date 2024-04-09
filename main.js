fb737186b91dfc6efea5b1829b8469e1fa3e6432
I585514 <ki.duk.kang@sap.com>
2024-04-09 19:25:31 +0200

    adding frontend

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form');
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener('submit', async (e) => {
+	e.preventDefault();
+	  showSpinner();
+	const data = new FormData(form);
+	const response = await fetch('http://localhost:8080/dream', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({prompt: data.get('prompt')}),
+		});
+
+	if (response.ok) {
+		const blob = await response.blob();
+		const url = URL.createObjectURL(blob);
+		const img = document.createElement('img');
+		img.src = url;
+
+		const result = document.querySelector('#result');
+		result.innerHTML = `<img src = "${url}" width='512' />`;
+	} else {
+		const err = await response.text();
+		alert(err);
+		console.error(err);
+	}
+
+	hideSpinner();
+
+});
+
+function showSpinner() {
+	const button = document.querySelector('button');
+	button.disabled = true;
+	button.innerHTML = 'Generating...<span class="spinner">🧙‍♂️</span>';
+	}
+
+function hideSpinner() {
+	const button = document.querySelector('button');
+	button.disabled = false;
+	button.innerHTML = 'Generate';
+}
